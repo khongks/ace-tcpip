29341b32f652a13193d990bee805163418d79e06
Kok Sing Khong <kskhong@kks-mbp16.modem>
2023-03-27 12:43:37 +1100

    Added deploy and test folders

---
+++ test/send.py
@@ -0,0 +1,39 @@
+import socket
+import argparse
+
+parser = argparse.ArgumentParser(description="This utility is used to send a message (read from a file) to TCPIP server endpoint",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("-s", "--server", help="IP addr or hostname of the TCPIP server")
+parser.add_argument("-p", "--port", help="Port of the TCPIP server")
+parser.add_argument("-f", "--file", help="provide a file that contains a XML message")
+args = parser.parse_args()
+config = vars(args)
+# print(config)
+
+
+# Define the server IP address and port number
+SERVER_IP = config.server
+SERVER_PORT = config.port
+
+# Create a TCP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect to the server
+sock.connect((SERVER_IP, SERVER_PORT))
+
+# Read the file content
+with open(config.file, 'r') as file:
+    message = file.read()
+file.close();
+
+# Send the message to the server
+print(f'Sending: {message}')
+sock.sendall(message.encode("utf-8"))
+print(f'Sent!');
+
+# print(f'Receiving ...')
+response = sock.recv(1024).decode("utf-8")
+print(f'Received: {response}')
+
+# Close the socket
+sock.close()
